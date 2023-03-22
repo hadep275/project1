330f19ac570fb9290ef7cd72d211a87200bba6be
hadep275 <hadep275@gmail.com>
2023-03-22 09:39:17 -0600

    working server

---
+++ server.js
@@ -0,0 +1,12 @@
+import express from "express";
+const app = express();
+
+const port = 2000;
+
+app.listen(port, (response, request) => {
+  console.log(`Started from the bottom now we here: ${port}`);
+});
+
+app.get("/", (request, response) => {
+  response.send("Fresher");
+});
