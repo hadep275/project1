54cd004642bd9412918f567f55b0c5946c47d856
hadep275 <hadep275@gmail.com>
2023-03-23 22:33:54 -0600

    api potentially added

--- server.js
+++ server.js
@@ -13,14 +13,31 @@ app.get("/", (request, response) => {
   response.send("Fresher");
 });
 
-app.get('/welcome', (req, res) => {
-  res.send('Welcome Back to the land of the living');
+app.get("/welcome", (req, res) => {
+  res.send("Welcome Back to the land of the living");
+});
+
+app.get("/about", (req, res) => {
+  res.send("What We Are All About");
+});
+
+app.put("/add", (req, res) => {
+  res.send("There There");
+});
+
+app.delete("/farewell", (req, res) => {
+  res.send("Odabo");
+});
+
+app.post("/submit", (req, res) => {
+  const data = req.body;
+  // TODO: process the submitted data
+  res.send("Final Destination");
 });
 
 app.post("/chat", (req, res) => {
   const message = req.body.message;
-  // TODO: process the user's message and generate a response
-  res.json({ message: "sjlsjaldkj" });
+  res.json({ message: "Back Again" });
 });
 
 app.listen(port, (response, request) => {
