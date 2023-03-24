1e2d41f03691b378ecd578ddc8018051c0a4f16d
hadep275 <hadep275@gmail.com>
2023-03-23 19:45:05 -0600

    added a new endpoint

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 import express from "express";
+import axios from "axios";
+import bodyParser from "body-parser";
+const API_URL = "http://localhost:2000/";
+
 const app = express();
 
 const port = 2000;
+
+app.use(bodyParser.json());
+
+app.get("/", (request, response) => {
+  response.send("Fresher");
+});
+
+app.get('/welcome', (req, res) => {
+  res.send('Welcome Back to the land of the living');
+});
+
+app.post("/chat", (req, res) => {
+  const message = req.body.message;
+  // TODO: process the user's message and generate a response
+  res.json({ message: "sjlsjaldkj" });
+});
 
 app.listen(port, (response, request) => {
   console.log(`Started from the bottom now we here: ${port}`);
 });
 
-app.get("/", (request, response) => {
-  response.send("Fresher");
-});
+
 
